2ae642ea29d6518c3ea4589a6c8899f44dac3b1c
Max Esser <max@esser.codes>
2024-03-16 13:44:21 -0500

    added font and fixed navbar

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link, Outlet } from "react-router-dom";
+import bulb from "/bulb.svg"
 
 function MainNav(props) {
 
     return (
         <div>
-            <Navbar>
+            <Navbar fixed="top">
+                <Navbar.Brand>
+                    <img
+                        alt="lightbulb logo"
+                        src={bulb}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />
+                </Navbar.Brand>{<div className="title">Max</div>}
                 <Container>
                     <Nav>
                         <Nav.Link as={Link} to="/">Home</Nav.Link>
