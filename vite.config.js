fa893ba08a87b53ad7181e2fe1e35e1a7d2b837e
Max Esser <max@esser.codes>
2024-03-15 22:41:18 -0500

    installed react router

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "essercodes.github.io",
+  base: "/essercodes.github.io",
   plugins: [react()],
 })
