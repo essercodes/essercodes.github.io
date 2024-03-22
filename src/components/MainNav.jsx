d904b07f9292acbf4f17c2df5e7156b8596f1856
Max Esser <max@esser.codes>
2024-03-22 00:06:38 -0500

    logo and decorative name responsively swapping

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,32 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link, Outlet } from "react-router-dom";
 import bulb from "/bulb.svg"
 
 function MainNav(props) {
 
+
     return (
+        
         <div>
-            <Navbar 
+            <Navbar
+                collapseOnSelect
+                expand="md"
                 fixed="top"
                 className="fixed-navbar"
+                variant="blue"
             >
+                    <div className="d-md-none m-l-1 title">Max</div>
                 <Navbar.Brand id="nav-logo">
                     <img
                         alt="lightbulb logo"
                         src={bulb}
                         width="45"
                         height="35"
-                        className="d-inline-block align-top"
                     />
-                </Navbar.Brand>{<div className="title">Max</div>}
-                <Container>
+                </Navbar.Brand>
+                    <div className="d-none d-md-block title">Max</div>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse>
                     <Nav>
-                        <Nav.Link as={Link} to="/">Home</Nav.Link>
-                        <Nav.Link as={Link} to="about">About Me</Nav.Link>
-                        <Nav.Link as={Link} to="resume">Resume</Nav.Link>
+                        <Nav.Link as={Link} to="/" eventKey={"home"}>Home</Nav.Link>
+                        <Nav.Link as={Link} to="about" eventKey={"about"}>About Me</Nav.Link>
+                        <Nav.Link as={Link} to="resume" eventKey={"resume"}>Resume</Nav.Link>
                     </Nav>
-                </Container>
+                </Navbar.Collapse>
             </Navbar>
             <div id="main-outlet">
                 <Outlet />
