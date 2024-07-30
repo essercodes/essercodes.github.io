800756e98f3a2b884fa151c1835098e7d576895e
Max Esser <max@esser.codes>
2024-07-29 20:48:01 -0500

    updated navbar to custom font with css hover and active options

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import {Navbar, Nav, Container, Row, Col, Button} from "react-bootstrap";
-import { Link, Outlet } from "react-router-dom";
+import {Link, Outlet} from "react-router-dom";
 import BulbSvg from "/public/bulb.svg?react";
 
 function MainNav(props) {
@@ -13,30 +13,24 @@ function MainNav(props) {
                 className="fixed-navbar"
                 variant="blue"
             >
-                <div className="mx-3 d-md-none title">Max</div>
-                <Navbar.Brand>
+                <Navbar.Brand href="/">
                     <Container>
                         <Row className="g-2">
-                            <Col>
-                                <BulbSvg className="logo" />
-                            </Col>
-                            <Col>
-                                <div className="d-none d-md-block title vertical-center">Max</div>
-                            </Col>
+                            <BulbSvg className="logo"/>
                         </Row>
                     </Container>
                 </Navbar.Brand>
-                <Navbar.Toggle className="mx-3" aria-controls="responsive-navbar-nav" />
+                <Navbar.Toggle className="mx-3 navbar-toggler" variant="dark" aria-controls="responsive-navbar-nav"/>
                 <Navbar.Collapse>
                     <Nav>
-                        <Nav.Link as={Link} to="/" eventKey={"home"}>Home</Nav.Link>
-                        <Nav.Link as={Link} to="about" eventKey={"about"}>About Me</Nav.Link>
-                        <Nav.Link as={Link} to="resume" eventKey={"resume"}>Resume</Nav.Link>
+                        <Nav.Link className="green-link" as={Link} to="/" eventKey={"home"}>Home</Nav.Link>
+                        <Nav.Link className="green-link" as={Link} to="about" eventKey={"about"}>About Me</Nav.Link>
+                        <Nav.Link className="green-link" as={Link} to="resume" eventKey={"resume"}>Resume</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
             <div id="main-outlet">
-                <Outlet />
+                <Outlet/>
             </div>
         </div>
     )
