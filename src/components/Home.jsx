685995a88ea822706b44c7b18e3d45fd6fd67926
Max Esser <max@esser.codes>
2024-08-01 22:16:42 -0500

    Add ThemeContext to multiple components
    Integrated ThemeContext into Resume, Home, and About components to enable dynamic theming. Updated the HTML structure to apply the theme class to the main body element for consistent styling across pages.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,11 +11,36 @@ function Home(props) {
 
     return <div className={`${theme} main-body`}>
         <sub>max@esser.codes</sub>
+        <br/>
+        <br/>
+        <h2>HOME CONTENT</h2>
         <div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget turpis sem. Curabitur vestibulum nibh sit amet lobortis iaculis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed sed nisl semper diam egestas vestibulum ac quis felis. Suspendisse vel viverra ipsum. In semper elit mi, id efficitur lacus suscipit vel. Mauris venenatis tellus non enim eleifend, lacinia rutrum enim porta. Quisque pellentesque hendrerit lacus, a posuere quam iaculis ut. Praesent bibendum nibh eu leo aliquam, non placerat sapien semper. Integer tincidunt enim sit amet odio laoreet suscipit. Vivamus sit amet dictum mi. Cras id interdum mi, nec lobortis ipsum. Pellentesque odio mi, lobortis vitae felis non, fermentum dignissim mauris. Duis sodales nibh at magna luctus, quis viverra nisi consectetur. Integer hendrerit nisl et turpis accumsan volutpat. Fusce euismod elit dui, sed mollis odio efficitur id.</p>
-            <p>Praesent elit libero, imperdiet ut consequat eu, rutrum pellentesque sem. Curabitur massa diam, elementum non risus ac, rutrum porta nisi. Quisque nec dignissim mi. Vestibulum velit leo, ullamcorper a odio sit amet, consequat consequat quam. Suspendisse blandit elit et arcu laoreet ultrices. Ut porta purus turpis, in lacinia augue ultricies sit amet. Nunc et imperdiet est. Nam aliquam enim vel turpis euismod bibendum. In ex enim, maximus non ipsum quis, vehicula fringilla risus. Fusce ut maximus erat.</p>
-            <p>Sed vel pulvinar ante. Nunc lobortis elit ante, ut gravida neque rutrum quis. Morbi vitae condimentum felis. In elementum dolor imperdiet ante mollis eleifend. Sed placerat enim nec euismod imperdiet. Proin id libero non magna tincidunt volutpat. Proin a condimentum dui, id gravida nibh. Sed at nisl quis nunc molestie bibendum. Aliquam sed tincidunt justo, nec posuere odio. Maecenas quis hendrerit ipsum. Duis sed condimentum magna.</p>
-            <p>Praesent finibus lacinia malesuada. Duis suscipit justo ante, et sollicitudin magna ullamcorper ac. Quisque venenatis mi risus, eu fringilla nisi ornare et. Donec id erat sed lorem rutrum laoreet. Mauris nec ex venenatis, lobortis sapien ac, mattis risus. Suspendisse id metus est. Morbi in odio non elit imperdiet finibus eleifend sed orci. Donec in porta tellus. Cras volutpat, urna et placerat tincidunt, leo metus dapibus nibh, non bibendum ante dui vitae dolor. Cras felis ligula, pretium vitae ultrices at, fringilla ac turpis. Sed lobortis nec dolor quis dictum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget turpis sem. Curabitur vestibulum
+                nibh sit amet lobortis iaculis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+                cubilia curae; Sed sed nisl semper diam egestas vestibulum ac quis felis. Suspendisse vel viverra ipsum.
+                In semper elit mi, id efficitur lacus suscipit vel. Mauris venenatis tellus non enim eleifend, lacinia
+                rutrum enim porta. Quisque pellentesque hendrerit lacus, a posuere quam iaculis ut. Praesent bibendum
+                nibh eu leo aliquam, non placerat sapien semper. Integer tincidunt enim sit amet odio laoreet suscipit.
+                Vivamus sit amet dictum mi. Cras id interdum mi, nec lobortis ipsum. Pellentesque odio mi, lobortis
+                vitae felis non, fermentum dignissim mauris. Duis sodales nibh at magna luctus, quis viverra nisi
+                consectetur. Integer hendrerit nisl et turpis accumsan volutpat. Fusce euismod elit dui, sed mollis odio
+                efficitur id.</p>
+            <p>Praesent elit libero, imperdiet ut consequat eu, rutrum pellentesque sem. Curabitur massa diam, elementum
+                non risus ac, rutrum porta nisi. Quisque nec dignissim mi. Vestibulum velit leo, ullamcorper a odio sit
+                amet, consequat consequat quam. Suspendisse blandit elit et arcu laoreet ultrices. Ut porta purus
+                turpis, in lacinia augue ultricies sit amet. Nunc et imperdiet est. Nam aliquam enim vel turpis euismod
+                bibendum. In ex enim, maximus non ipsum quis, vehicula fringilla risus. Fusce ut maximus erat.</p>
+            <p>Sed vel pulvinar ante. Nunc lobortis elit ante, ut gravida neque rutrum quis. Morbi vitae condimentum
+                felis. In elementum dolor imperdiet ante mollis eleifend. Sed placerat enim nec euismod imperdiet. Proin
+                id libero non magna tincidunt volutpat. Proin a condimentum dui, id gravida nibh. Sed at nisl quis nunc
+                molestie bibendum. Aliquam sed tincidunt justo, nec posuere odio. Maecenas quis hendrerit ipsum. Duis
+                sed condimentum magna.</p>
+            <p>Praesent finibus lacinia malesuada. Duis suscipit justo ante, et sollicitudin magna ullamcorper ac.
+                Quisque venenatis mi risus, eu fringilla nisi ornare et. Donec id erat sed lorem rutrum laoreet. Mauris
+                nec ex venenatis, lobortis sapien ac, mattis risus. Suspendisse id metus est. Morbi in odio non elit
+                imperdiet finibus eleifend sed orci. Donec in porta tellus. Cras volutpat, urna et placerat tincidunt,
+                leo metus dapibus nibh, non bibendum ante dui vitae dolor. Cras felis ligula, pretium vitae ultrices at,
+                fringilla ac turpis. Sed lobortis nec dolor quis dictum.</p>
         </div>
     </div>
 }
