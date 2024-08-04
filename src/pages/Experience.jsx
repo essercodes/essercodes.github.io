6f42c10ba28070e51fc1c5eff18f6065feb6fbdd
Max Esser <max@esser.codes>
2024-08-04 16:37:03 -0500

    Update experience and about pages to 'Under Construction'
    Replaced placeholder content on the Experience and About pages with "Under Construction" headers. This change signals ongoing development for these sections.

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -3,7 +3,7 @@ import Page from "../components/Page.jsx";
 
 function Experience() {
     return <Page compactTitle="Exp" path="experience">
-        <h1>RESUME CONTENT</h1>
+        <h1>Under Construction</h1>
         <div>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget turpis sem. Curabitur vestibulum
                 nibh sit amet lobortis iaculis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
