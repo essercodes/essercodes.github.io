69491e0211549908489b6a5facf680f8cf4cf24f
Max Esser <max@esser.codes>
2024-08-04 17:59:07 -0500

    Update link attributes and refine placeholder text
    Set external links to open in a new tab with added security attributes. Additionally, replaced placeholder text in `Experience.jsx` with a more varied placeholder content and adjusted paragraph formatting in `About.jsx`.

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -5,32 +5,52 @@ function Experience() {
     return <Page compactTitle="Exp" path="experience">
         <h1>Under Construction</h1>
         <div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget turpis sem. Curabitur vestibulum
-                nibh sit amet lobortis iaculis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
-                cubilia curae; Sed sed nisl semper diam egestas vestibulum ac quis felis. Suspendisse vel viverra ipsum.
-                In semper elit mi, id efficitur lacus suscipit vel. Mauris venenatis tellus non enim eleifend, lacinia
-                rutrum enim porta. Quisque pellentesque hendrerit lacus, a posuere quam iaculis ut. Praesent bibendum
-                nibh eu leo aliquam, non placerat sapien semper. Integer tincidunt enim sit amet odio laoreet suscipit.
-                Vivamus sit amet dictum mi. Cras id interdum mi, nec lobortis ipsum. Pellentesque odio mi, lobortis
-                vitae felis non, fermentum dignissim mauris. Duis sodales nibh at magna luctus, quis viverra nisi
-                consectetur. Integer hendrerit nisl et turpis accumsan volutpat. Fusce euismod elit dui, sed mollis odio
-                efficitur id.</p>
-            <p>Praesent elit libero, imperdiet ut consequat eu, rutrum pellentesque sem. Curabitur massa diam, elementum
-                non risus ac, rutrum porta nisi. Quisque nec dignissim mi. Vestibulum velit leo, ullamcorper a odio sit
-                amet, consequat consequat quam. Suspendisse blandit elit et arcu laoreet ultrices. Ut porta purus
-                turpis, in lacinia augue ultricies sit amet. Nunc et imperdiet est. Nam aliquam enim vel turpis euismod
-                bibendum. In ex enim, maximus non ipsum quis, vehicula fringilla risus. Fusce ut maximus erat.</p>
-            <p>Sed vel pulvinar ante. Nunc lobortis elit ante, ut gravida neque rutrum quis. Morbi vitae condimentum
-                felis. In elementum dolor imperdiet ante mollis eleifend. Sed placerat enim nec euismod imperdiet. Proin
-                id libero non magna tincidunt volutpat. Proin a condimentum dui, id gravida nibh. Sed at nisl quis nunc
-                molestie bibendum. Aliquam sed tincidunt justo, nec posuere odio. Maecenas quis hendrerit ipsum. Duis
-                sed condimentum magna.</p>
-            <p>Praesent finibus lacinia malesuada. Duis suscipit justo ante, et sollicitudin magna ullamcorper ac.
-                Quisque venenatis mi risus, eu fringilla nisi ornare et. Donec id erat sed lorem rutrum laoreet. Mauris
-                nec ex venenatis, lobortis sapien ac, mattis risus. Suspendisse id metus est. Morbi in odio non elit
-                imperdiet finibus eleifend sed orci. Donec in porta tellus. Cras volutpat, urna et placerat tincidunt,
-                leo metus dapibus nibh, non bibendum ante dui vitae dolor. Cras felis ligula, pretium vitae ultrices at,
-                fringilla ac turpis. Sed lobortis nec dolor quis dictum.</p>
+            <p>
+                Lorem ipsum odor amet, consectetuer adipiscing elit. Nibh amet malesuada hendrerit semper vivamus cubilia
+                vestibulum massa. Platea maecenas dis elementum elementum natoque mus mi laoreet! Ante consectetur dui
+                potenti vel justo. At posuere odio erat venenatis magna netus; natoque dapibus. Magnis a cras interdum
+                aptent maecenas dapibus leo fusce hendrerit. Auctor suscipit parturient ex ultrices tortor tristique. Ac
+                diam auctor eu justo etiam ex fusce quis. Maecenas nisl at nisl efficitur ut iaculis ex integer
+                nascetur.
+            </p>
+            <p>
+                Amaecenas quisque ex maecenas dictum morbi curabitur. Amet rutrum inceptos facilisi semper faucibus
+                vehicula
+                dictum. Id consequat ut nisi habitant tortor ante. Malesuada luctus nulla odio dignissim arcu praesent.
+                Phasellus nisi penatibus vehicula aenean sociosqu turpis. Consectetur mi faucibus erat ullamcorper
+                consectetur leo aenean proin. Fusce sit erat himenaeos aenean velit dictum lobortis. Donec augue augue
+                vestibulum nam et. Commodo maecenas magnis iaculis metus; nisl quisque netus amet ligula. Nunc purus
+                hendrerit inceptos mus elementum posuere netus.
+            </p>
+            <p>
+                Sagittis donec dis curabitur ante mattis nullam mi rhoncus inceptos. Mauris urna ridiculus inceptos
+                vehicula
+                sed inceptos a nam. Ante praesent pretium ullamcorper purus proin interdum, lacus posuere. Leo felis
+                lacinia
+                enim vel penatibus. Ac scelerisque sed blandit nascetur dolor sociosqu gravida vel. Sit semper
+                adipiscing
+                sem vitae sem. Vehicula rutrum volutpat proin nulla fusce fusce nostra leo elit. Ornare maecenas dui
+                iaculis
+                ante varius nisl facilisis maecenas.
+            </p>
+            <p>
+                Cursus nam dui ligula faucibus eget a. Conubia quisque taciti in cras laoreet risus. Sapien nisi lacus
+                per
+                purus porta sollicitudin. Sapien vestibulum euismod varius, magna accumsan rutrum ante ligula. Facilisis
+                proin accumsan vulputate bibendum semper nullam. Placerat odio convallis scelerisque sodales taciti
+                aptent.
+            </p>
+            <p>
+                Mus ornare velit; ex lacinia ornare mollis himenaeos vivamus. Maecenas libero placerat venenatis; donec
+                dignissim massa rhoncus. Lacinia lobortis volutpat vehicula; rutrum convallis consectetur libero. Neque
+                dui
+                habitant hac commodo leo. Sapien dui integer quis massa himenaeos accumsan. Egestas dolor condimentum
+                augue
+                conubia iaculis vivamus dui semper. Convallis porta vestibulum tempus, suspendisse nec elit consequat.
+                Torquent cubilia netus libero hendrerit habitant a luctus. Pulvinar imperdiet ornare tincidunt
+                porttitor;
+                dolor neque cras.
+            </p>
         </div>
     </Page>
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import '../css/pages/home.css';
 
-import React, {useContext} from "react";
 import Page from "../components/Page.jsx";
 import {Card, Col, Container, Row} from "react-bootstrap";
 
@@ -12,7 +11,7 @@ import MailIcon from "bootstrap-icons/icons/mailbox.svg?react"
 class ContactCard extends React.Component {
     render() {
         return <Col xs={4} md={3} lg={2} className="contact-card">
-            <Card as="a" href={this.props.href} className="link-card mx-auto">
+            <Card as="a" href={this.props.href} target="_blank" rel="noopener noreferrer" className="link-card mx-auto">
                 <Card.Img as={this.props.as} alt={this.props.alt} className="contact-icon"/>
             </Card>
         </Col>;
