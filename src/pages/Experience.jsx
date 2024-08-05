0c22120bc56765fd28a213217fa48741694c9d48
Max Esser <max@esser.codes>
2024-08-04 21:23:10 -0500

    Remove unused packages and add new dependencies
    Unused packages such as `gh-pages` and `sort-by` were removed to clean up the dependency list. New dependencies including `webpack` and several `@webassemblyjs` packages were added to support new functionalities.

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,67 +1,57 @@
-import React, {useState} from "react";
+import React from "react";
 import Page from "../components/Page.jsx";
-import {Alert, Button} from "react-bootstrap";
+import {Card, Col, Row, Stack, Tab, Tabs} from "react-bootstrap";
+
+import work_exp from "./exp_data/work_exp.json";
 
 function Experience() {
-    const [show, setShow] = useState(true);
     return <Page compactTitle="Exp" path="experience">
-        <Alert show={show} variant="success">
-            <Alert.Heading>Under Construction!</Alert.Heading>
-            <div className="d-flex justify-content-end">
-                <Button onClick={() => setShow(false)} variant="outline-success">
-                    Close me
-                </Button>
-            </div>
-        </Alert>
-        <div>
-            <p>
-                Lorem ipsum odor amet, consectetuer adipiscing elit. Nibh amet malesuada hendrerit semper vivamus
-                cubilia
-                vestibulum massa. Platea maecenas dis elementum elementum natoque mus mi laoreet! Ante consectetur dui
-                potenti vel justo. At posuere odio erat venenatis magna netus; natoque dapibus. Magnis a cras interdum
-                aptent maecenas dapibus leo fusce hendrerit. Auctor suscipit parturient ex ultrices tortor tristique. Ac
-                diam auctor eu justo etiam ex fusce quis. Maecenas nisl at nisl efficitur ut iaculis ex integer
-                nascetur.
-            </p>
-            <p>
-                Amaecenas quisque ex maecenas dictum morbi curabitur. Amet rutrum inceptos facilisi semper faucibus
-                vehicula
-                dictum. Id consequat ut nisi habitant tortor ante. Malesuada luctus nulla odio dignissim arcu praesent.
-                Phasellus nisi penatibus vehicula aenean sociosqu turpis. Consectetur mi faucibus erat ullamcorper
-                consectetur leo aenean proin. Fusce sit erat himenaeos aenean velit dictum lobortis. Donec augue augue
-                vestibulum nam et. Commodo maecenas magnis iaculis metus; nisl quisque netus amet ligula. Nunc purus
-                hendrerit inceptos mus elementum posuere netus.
-            </p>
-            <p>
-                Sagittis donec dis curabitur ante mattis nullam mi rhoncus inceptos. Mauris urna ridiculus inceptos
-                vehicula
-                sed inceptos a nam. Ante praesent pretium ullamcorper purus proin interdum, lacus posuere. Leo felis
-                lacinia
-                enim vel penatibus. Ac scelerisque sed blandit nascetur dolor sociosqu gravida vel. Sit semper
-                adipiscing
-                sem vitae sem. Vehicula rutrum volutpat proin nulla fusce fusce nostra leo elit. Ornare maecenas dui
-                iaculis
-                ante varius nisl facilisis maecenas.
-            </p>
-            <p>
-                Cursus nam dui ligula faucibus eget a. Conubia quisque taciti in cras laoreet risus. Sapien nisi lacus
-                per
-                purus porta sollicitudin. Sapien vestibulum euismod varius, magna accumsan rutrum ante ligula. Facilisis
-                proin accumsan vulputate bibendum semper nullam. Placerat odio convallis scelerisque sodales taciti
-                aptent.
-            </p>
-            <p>
-                Mus ornare velit; ex lacinia ornare mollis himenaeos vivamus. Maecenas libero placerat venenatis; donec
-                dignissim massa rhoncus. Lacinia lobortis volutpat vehicula; rutrum convallis consectetur libero. Neque
-                dui
-                habitant hac commodo leo. Sapien dui integer quis massa himenaeos accumsan. Egestas dolor condimentum
-                augue
-                conubia iaculis vivamus dui semper. Convallis porta vestibulum tempus, suspendisse nec elit consequat.
-                Torquent cubilia netus libero hendrerit habitant a luctus. Pulvinar imperdiet ornare tincidunt
-                porttitor;
-                dolor neque cras.
-            </p>
-        </div>
+        <Tabs
+            defaultActiveKey={"work"}
+            id="uncontrolled-tab-example"
+            className="mb-3"
+        >
+            <Tab className={"px-2"} eventKey="work" title="Work">
+                <Stack>
+                    {work_exp.map(exp => {
+                        return <Card className={"p-3 m-2"} key={`${exp.where}-${exp.when}`}>
+                            <Card.Title>{exp.where}</Card.Title>
+                            <Card.Subtitle>
+                                <Row>
+                                    <Col>{exp.title}</Col>
+                                    <Col>{exp.when}</Col>
+                                </Row>
+                            </Card.Subtitle>
+                            <Card.Body className={"px-4"}>
+                                <Row>
+                                    {exp.description}
+                                </Row>
+                                <Row className={"pt-3"}>
+                                    {<ul>
+                                        {exp.bullets.map((b, i) => {
+                                                return <li key={`${exp.where}-${exp.when}-${i}`}>
+                                                    {b}
+                                                </li>
+                                            }
+                                        )}
+                                    </ul>}
+                                </Row>
+                            </Card.Body>
+                        </Card>
+                    })}
+                </Stack>
+            </Tab>
+            <Tab eventKey="edu" title="Education">
+
+            </Tab>
+            <Tab eventKey="skills" title="Skills">
+
+            </Tab>
+            <Tab eventKey="projects" title="Projects">
+
+            </Tab>
+
+        </Tabs>
     </Page>
 }
 
