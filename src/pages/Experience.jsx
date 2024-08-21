fa42927a602e22ba0cdfc6ce2ed47b802d1ad2a3
Max Esser <max@esser.codes>
2024-08-21 12:37:41 -0500

    Remove "Under Construction" alert from Experience page.
    The alert informing users that the Experience page is under construction has been removed. This simplifies the user interface and eliminates unnecessary clutter.

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,23 +1,13 @@
 import './experience.css';
 
-import React, {useEffect, useState} from "react";
+import React from "react";
 import Page from "../components/Page.jsx";
-import {Alert, Button, Card, Col, Container, Row} from "react-bootstrap";
 
 import exp_data from "./Experience/experience.json";
 import Timeline from "./Experience/Timeline.jsx";
 
 function Experience() {
-    const [show, setShow] = useState(true);
     return <Page compactTitle="Exp" path="experience">
-        <Alert show={show} variant="success">
-            <Alert.Heading>Under Construction!</Alert.Heading>
-            <div className="d-flex justify-content-end">
-                <Button onClick={() => setShow(false)} variant="outline-success">
-                    Close me
-                </Button>
-            </div>
-        </Alert>
         <Timeline data={exp_data}/>
     </Page>
 }
